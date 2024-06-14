5980658538f0c52b2651ddf73152e914648f23c1
DivyaDarshanTiwari <davidrocco400@outlook.com>
2024-06-14 17:28:46 +0530

    2nd cmmit after css added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BitCoinPageComponent } from './bit-coin-page/bit-coin-page.component';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
+
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
+import {NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BitCoinPageComponent
+    BitCoinPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
